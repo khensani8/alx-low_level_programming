88e2f05f7eab2fa785d059a4530119f75f2b6bed
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-04-24 12:51:36 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *main - Entry Point
+  *Description: 'print all numbers of base 16 in lowercase'
+  *Return: Always 0
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; n++)
+	{
+		putchar(m);
+	}
+	return (0);
+}
