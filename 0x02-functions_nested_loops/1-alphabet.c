21cda0dd10bddf81066cc2a6f3f4c339791eab24
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-04-25 09:20:05 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
  *Description: 'Prints the alphabet in lowercase'
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	int i;
 
 	for (i = 97; i <= 122; i++)
 	{
-		_putchar(i);
+		_putchar(i)
 	}
 	_putchar('\n');
 	return (0);
