7ad520ab6db06f6b5ede3727c2eec871074375cf
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-05-08 09:45:47 -0700

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  *
+  *_memset - fill a bloack of memory with a value
+  *@s: starting address of memory to be filled
+  *@b: desired value 
+  *@n: number of bites to be changed
+  *
+  *Return: changed array with new value for n bytes
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
+
