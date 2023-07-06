67b01e6f552f765502c9d9efb302078ad63348a1
Khensani.Ngobeni <khensi.ngobeni@gmail.com>
2023-07-06 01:54:55 -0700

    Task 0

---
+++ 0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int j;
+	unsigned int deci_val = 0;
+
+	if (!b)
+		return (0);
+
+	for (j = 0; b[j]; j++)
+	{
+		if (b[j] < '0' || b[j] > '1')
+			return (0);
+		deci_val = 2 * deci_val + (b[j] - '0');
+	}
+
+	return (deci_val);
+
